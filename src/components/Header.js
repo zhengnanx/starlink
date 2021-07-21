80bb2550cdecd21c394fc46e89e99ebb9d3812c7
zhengnanx <zhengnanx87@gmail.com>
2021-07-21 11:07:19 -0700

    spaceX -1

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={} className="App-logo" alt="logo">
+          <p className="title"> StarLink Tracker</p>
+        </img>
+      </header>
+    );
+  }
+}
+
+export default Header;
